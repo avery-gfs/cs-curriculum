6a356d49bc76ee3951a566708a52ecd97bac2648
Avery <anortonsmith@germantownfriends.org>
2026-09-09 13:08:24 -0400

    update

--- cs-3/01-recursion/grid/reference.py
+++ cs-3/01-recursion/grid/reference.py
@@ -1,6 +1,11 @@
 letters = "abcdefghij"
 
-for i in range(10):
-    for j in range(10):
-        print(f"{letters[i]}{j}", end=" ")
-    print()
+
+def printGrid():
+    for i in range(10):
+        for j in range(10):
+            print(f"{letters[i]}{j}", end=" ")
+        print()
+
+
+printGrid()
