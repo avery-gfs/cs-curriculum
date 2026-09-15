718b5c60da5f6970ea289cf199e778845e175aa7
Avery <anortonsmith@germantownfriends.org>
2026-09-15 12:39:02 -0400

    update problems

--- cs-3/02-recursion-lists/problem.py
+++ cs-3/02-recursion-lists/problem.py
@@ -8,14 +8,14 @@ def product(numbers):
 print(product([9, 4, 5, 6, 8, 10]))  # Should print 86400
 
 
-def contains(items, value):
-    # Check if a list of items contains a value recursively
+def hasSeven(numbers):
+    # Check if a list of numbers contains the number 7
 
     pass
 
 
-print(contains([9, 4, 5, 6, 8, 10], 8))  # Should print True
-print(contains([9, 4, 5, 6, 8, 10], 7))  # Should print False
+print(hasSeven([9, 4, 5, 6, 8, 10, 7]))  # Should print True
+print(hasSeven([9, 4, 5, 6, 8, 10]))  # Should print False
 
 
 def minimum(numbers):
