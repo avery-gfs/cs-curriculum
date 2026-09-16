94f17e9bbf619909479585ade37315d52ce88cc6
Avery <anortonsmith@germantownfriends.org>
2026-09-16 15:09:39 -0400

    update

--- cs-3/03-binary-search/words/solved.py
+++ cs-3/03-binary-search/words/solved.py
@@ -1,40 +1,74 @@
-import time
-import random
-from contextlib import contextmanager
+# import time
 
+# with open("words-sorted.txt") as file:
+# 	words = file.read().splitlines()
 
-@contextmanager
-def timer(name):
-    print(name)
-    start = time.time_ns()
-    yield
-    end = time.time_ns()
-    print(end - start)
+# def search(lst, target):
+#     if lst == []:
+#         return False
 
+#     mid = len(lst) // 2
 
-def search(lst, low, high, target):
-    if low >= high:
-        return False
+#     if lst[mid] == target:
+#         return True
 
-    mid = (low + high) // 2
+#     if lst[mid] < target:
+#         return search(lst[mid + 1 :], target)
 
-    if lst[mid] == target:
-        return True
+#     return search(lst[:mid], target)
 
-    if lst[mid] < target:
-        return search(lst, mid + 1, high, target)
+# start = time.time_ns()
 
-    return search(lst, low, mid - 1, target)
+# print("asdf" in words)
 
+# end = time.time_ns()
 
-count = 100_000
-maxNum = 200_000
+# print(end - start)
 
-numbers = sorted([random.randrange(1, maxNum + 1) for _ in range(count)])
-target = random.randrange(1, maxNum + 1)
+# import time
+# import random
 
-with timer("linear search"):
-    target in numbers
+# count = 10**8
+# maxNum = 10**9
 
-with timer("binary search"):
-    search(numbers, 0, len(numbers), target)
+# numbers = []
+
+# for _ in range(count):
+# 	n = random.randrange(1, maxNum + 1)
+# 	numbers.append(n)
+
+# numbers.sort()
+
+# def search(lst, target):
+#     if lst == []:
+#         return False
+
+#     mid = len(lst) // 2
+
+#     if lst[mid] == target:
+#         return True
+
+#     if lst[mid] < target:
+#         return search(lst[mid + 1 :], target)
+
+#     return search(lst[:mid], target)
+
+# target = random.randrange(1, maxNum + 1)
+
+# start = time.time()
+
+# for i in numbers:
+# 	if i == target:
+# 		print(True)
+
+# end = time.time()
+
+# print(end - start)
+
+# start = time.time()
+
+# print(search(numbers, target))
+
+# end = time.time()
+
+# print(end - start)
