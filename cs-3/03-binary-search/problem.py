20fea0c7314d52974b05dec68240b4553cd91b5d
Avery <anortonsmith@germantownfriends.org>
2026-09-16 13:12:21 -0400

    update problems

---
+++ cs-3/03-binary-search/problem.py
@@ -0,0 +1,8 @@
+def search(lst, target):
+    pass
+
+
+lst = [2, 5, 24, 32, 33, 34, 35, 38, 38, 41, 49, 74, 79, 87, 91, 98, 98, 99]
+
+print(search(lst, 74))  # Should print True
+print(search(lst, 75))  # Should print False
