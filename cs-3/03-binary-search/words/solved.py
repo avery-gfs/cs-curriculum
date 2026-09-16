e546d1c561ab66235ede7abd75b822811c823398
Avery <anortonsmith@germantownfriends.org>
2026-09-16 15:12:27 -0400

    update

--- cs-3/03-binary-search/words/solved.py
+++ cs-3/03-binary-search/words/solved.py
@@ -27,89 +27,11 @@ def search(lst, low, high, target):
     return search(lst, low, mid - 1, target)
 
 
-count = 100_000
-maxNum = 200_000
-
-numbers = sorted([random.randrange(1, maxNum + 1) for _ in range(count)])
-target = random.randrange(1, maxNum + 1)
+with open("words-sorted.txt") as file:
+    words = file.read().splitlines()
 
 with timer("linear search"):
-    target in numbers
+    "asdf" in words
 
 with timer("binary search"):
-    search(numbers, 0, len(numbers) - 1, target)
-
-# import time
-
-# with open("words-sorted.txt") as file:
-# 	words = file.read().splitlines()
-
-# def search(lst, target):
-#     if lst == []:
-#         return False
-
-#     mid = len(lst) // 2
-
-#     if lst[mid] == target:
-#         return True
-
-#     if lst[mid] < target:
-#         return search(lst[mid + 1 :], target)
-
-#     return search(lst[:mid], target)
-
-# start = time.time_ns()
-
-# print("asdf" in words)
-
-# end = time.time_ns()
-
-# print(end - start)
-
-# import time
-# import random
-
-# count = 10**8
-# maxNum = 10**9
-
-# numbers = []
-
-# for _ in range(count):
-# 	n = random.randrange(1, maxNum + 1)
-# 	numbers.append(n)
-
-# numbers.sort()
-
-# def search(lst, target):
-#     if lst == []:
-#         return False
-
-#     mid = len(lst) // 2
-
-#     if lst[mid] == target:
-#         return True
-
-#     if lst[mid] < target:
-#         return search(lst[mid + 1 :], target)
-
-#     return search(lst[:mid], target)
-
-# target = random.randrange(1, maxNum + 1)
-
-# start = time.time()
-
-# for i in numbers:
-# 	if i == target:
-# 		print(True)
-
-# end = time.time()
-
-# print(end - start)
-
-# start = time.time()
-
-# print(search(numbers, target))
-
-# end = time.time()
-
-# print(end - start)
+    search(words, 0, len(words) - 1, "asdf")
