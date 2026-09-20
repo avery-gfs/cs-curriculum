dbd4608ba3e6d8053dfa8b79c448dd93a8ba8afa
Avery <anortonsmith@germantownfriends.org>
2026-09-20 19:30:48 -0400

    update problems

---
+++ cs-3/04-sorting/bubble/visualize.py
@@ -0,0 +1,19 @@
+nums = [1, 3, 2, 5, 1, 4, 1]
+
+
+def render(index):
+    print("---\n\n```")
+    print(" ".join(str(n) for n in nums))
+    print(" " * (2 * index) + "^ ^")
+    print("```\n")
+
+
+for end in range(len(nums) - 1, 0, -1):
+    for index in range(end):
+        render(index)
+
+        if nums[index] > nums[index + 1]:
+            tmp = nums[index]
+            nums[index] = nums[index + 1]
+            nums[index + 1] = tmp
+            render(index)
