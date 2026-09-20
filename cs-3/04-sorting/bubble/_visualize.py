3a0cb6887ff9ad6a34c6e1d779d874e798a46ef7
Avery <anortonsmith@germantownfriends.org>
2026-09-20 19:10:17 -0400

    update

---
+++ cs-3/04-sorting/bubble/_visualize.py
@@ -0,0 +1,23 @@
+items = list("🦀🥦🫖🐼🧲🐼")
+
+
+def render(index):
+    left = "   ".join(items[:index])
+
+    if index:
+        left = "   " + left
+
+    right = "   ".join(items[index + 2 :])
+    middle = f" ( {items[index]}  {items[index + 1]} ) "
+    print(left + middle + right)
+
+
+for end in range(len(items) - 1, 0, -1):
+    for index in range(end):
+        render(index)
+
+        if items[index] > items[index + 1]:
+            tmp = items[index]
+            items[index] = items[index + 1]
+            items[index + 1] = tmp
+            render(index)
