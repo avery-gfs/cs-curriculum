932879013076f247d8b1bbd6bf9ab232744e1ce0
Avery <anortonsmith@germantownfriends.org>
2026-09-21 22:05:41 -0400

    update problems

---
+++ cs-3/04-sorting/bubble/problem.py
@@ -0,0 +1,18 @@
+def bubbleSort(items):
+    pass
+
+
+numbers = [8, 7, 12, 4, 10, 4, 3, 12, 11, 5, 2, 1, 6, 7, 9, 12]
+
+bubbleSort(numbers)
+print(numbers)
+
+emojis = list("🦀🥦🫖🐼🧲🐼🏀🫖🪭💩🍄⚽🥑🥦🦘🫖")
+
+bubbleSort(emojis)
+print(emojis)
+
+words = "Mayday Mayday watch the needle leave the dial".split()
+
+bubbleSort(words)
+print(words)
