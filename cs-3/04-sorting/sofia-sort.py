6ef13a92f3586f747cfcb3aacf8a47d7e898250c
Avery <anortonsmith@germantownfriends.org>
2026-09-22 15:47:45 -0400

    update

---
+++ cs-3/04-sorting/sofia-sort.py
@@ -0,0 +1,20 @@
+def render(items, i, j):
+    print("```")
+    print(" ".join(map(str, items)))
+    print("  " * j + "^")
+    print("  " * i + "^")
+    print("```\n---\n")
+
+
+def sofiaSort(items):
+    for i in range(len(items)):
+        for j in range(i, len(items)):
+            render(items, i, j)
+            if items[i] > items[j]:
+                tmp = items[i]
+                items[i] = items[j]
+                items[j] = tmp
+                render(items, i, j)
+
+
+sofiaSort([5, 3, 2, 5, 1, 4, 1])
